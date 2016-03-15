a0bbe1e4feeae9a98f2e51e544340a42fe444ada
notori0us <cbwcjw@gmail.com>
2016-03-15 02:16:35 -0400

    Figure the horus and count in the VIEW

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,20 +9,6 @@ class UserMailer < ActionMailer::Base
 
 	def send_digest(logs, email)
 		@logs = logs
-		@cs_hours = 0
-		@ges_events = 0
-		@lte_events = 0
-
-		@logs.each do |l|
-
-			if l.category == "CS"
-				@cs_hours += l.length
-			elsif l.category == "GES"
-				@ges_events += 1
-			elsif l.category== "LTE"
-				@lte_events += 1
-			end
-		end
 
 		@email = email
 		mail(:to => "<#{email}>", :subject => "GES Log History")
