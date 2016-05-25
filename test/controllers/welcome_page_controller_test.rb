435577ec734568c24a44248e4e9554a7fad5fbb1
notori0us <cbwcjw@gmail.com>
2016-05-25 01:48:49 -0400

    add functional test as part of #10

--- test/controllers/welcome_page_controller_test.rb
+++ test/controllers/welcome_page_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class WelcomePageControllerTest < ActionController::TestCase
-  test "should get welcome" do
-    get :welcome
-    assert_response :success
-  end
+	# test the homepage, does it load the welcome template?
+	# does it load the navbar / application layout?
+	test "should get welcome" do
+		get :welcome
+		assert_template :welcome
+		assert_template layout: "layouts/application"
+		assert_response :success
+	end
 
 end
